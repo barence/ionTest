d381b9bc9ab74e72d3fd009858bb93503acf2d2a
barence <44985626+barence@users.noreply.github.com>
2024-09-17 16:10:43 -0700

    Update ionTest.js

--- ionTest.js
+++ ionTest.js
@@ -7,6 +7,7 @@ const ions = [
     ["sulfate ion","SO₄⁻²"],
     ["bromate ion","BrO₃⁻¹"],
     ["fluoride ion","F⁻¹"],
+    ["calcium ion","Ca⁺²"],
     ["cuprous ion","Cu⁺¹"],
     ["ammonium ion","NH₄⁺¹"],
     ["oxide ion","O⁻²"],
@@ -50,7 +51,7 @@ let firstTime = true;
 let savedmiss = "none!";
 let firstMistake =true;
 let done = document.getElementById("popup");
-const order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41];
+const order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
 function subOptimalScript(){
     let unmodified = document.getElementById("input").value;
     let length = unmodified.length;
